9dfac92d0dbd7b2ecd98b024532a127512273396
yvikentiev <yvikentiev@gmail.com>
2021-02-03 20:28:08 +0700

    commit2
    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        // commit1
+        // commit2
+        // commit3
     }
 }
